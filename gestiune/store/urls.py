9e815b82e1d34ddb57430582318690cebc9e71be
ScarKing123 <vrvevdfr@gmail.com>
2024-03-19 00:50:25 +0200

    Now items can be added to cart

--- gestiune/store/urls.py
+++ gestiune/store/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home , name='home'),
     path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>', views.productpage , name='product_detail'),
-    path('cart/', views.cart, name='cart'),
+    path('<slug:category_slug>/<slug:product_slug>',views.productpage , name='product_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/', views.cart_detail, name='cart_detail'),
 ]
