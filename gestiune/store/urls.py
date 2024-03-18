3e1787fa382419f05732d0fb76e91e991c5e39db
ScarKing123 <vrvevdfr@gmail.com>
2024-03-18 02:58:02 +0200

    HomePage and ProductPage are  now linked

--- gestiune/store/urls.py
+++ gestiune/store/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home , name='home'),
     path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('product/', views.productpage , name='product'),
+    path('<slug:category_slug>/<slug:product_slug>', views.productpage , name='product_detail'),
     
 ]
