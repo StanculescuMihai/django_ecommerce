270decbfa2cb001255bac55477eb68fca2b8f660
Stanculescu Mihai <mstanculescu2004@yahoo.com>
2024-10-19 14:07:22 +0300

    settings.py

--- gestiune/gestiune/settings.py
+++ gestiune/gestiune/settings.py
@@ -132,3 +132,5 @@ CRISPY_ALLOWED_TEMPLATE_PACKS= 'bootstrap5'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STRIPE_SECRET_KEY ="..."
+STRIPE_PUBLISHABLE_KEY="..."
